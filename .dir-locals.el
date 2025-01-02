c0f23b56837d09f4b4fe7a5e7ac7ad127a01f4dc
Vedang Manerikar <ved.manerikar@gmail.com>
2025-01-02 14:57:23 +0530

    Create zprint and dir-locals files for formatting

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((clojure-dart-ts-mode . ((apheleia-formatter . (zprint))))
+ (clojure-jank-ts-mode . ((apheleia-formatter . (zprint))))
+ (clojure-mode . ((apheleia-formatter . (zprint))))
+ (clojure-ts-mode . ((apheleia-formatter . (zprint))))
+ (clojurec-mode . ((apheleia-formatter . (zprint))))
+ (clojurec-ts-mode . ((apheleia-formatter . (zprint))))
+ (clojurescript-mode . ((apheleia-formatter . (zprint))))
+ (clojurescript-ts-mode . ((apheleia-formatter . (zprint)))))
